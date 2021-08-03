6393cd366996f181e78021a132954225547a77c7
Xiaonan Xu <hello_xxn@163.com>
2021-08-03 19:24:23 +0800

    add part of log function

--- diawe/urls.py
+++ diawe/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('article/', views.log, name='article'),
+    path('detail/<int:id>/', views.detail, name='detail'),
+    path('create/', views.article_create, name='create'),
 ]
--- diawe/views.py
+++ diawe/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from diawe.forms import  UserForm, UserProfileForm
+from diawe.forms import  UserForm, UserProfileForm,LogForm
 from datetime import datetime
-
-
+from diawe.models import LogPost, UserProfile
+from django.contrib.auth.models import User
 
 
 @login_required
@@ -56,7 +56,7 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
-                return redirect(reverse('diawe:index'))
+                return redirect(reverse('diawe:article'))
             else:
 
                 return HttpResponse("Your Rango account is disabled.")
@@ -93,3 +93,46 @@ def get_server_side_cookie(request, cookie, default_val=None):
     if not val:
         val = default_val
     return val
+
+
+def log(request):
+    articles = LogPost.objects.all()
+    # 需要传递给模板（templates）的对象
+    context = { 'articles': articles }
+    # render函数：载入模板，并返回context对象
+    return render(request, 'diawe/article.html', context)
+
+def detail(request,id):
+    log = LogPost.objects.get(id=id)
+    context = {'article':log}
+    return render(request,'diawe/detail.html',context)
+
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        log_form = LogForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if log_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = log_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # 如果你进行过删除数据表的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入此用户的id
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("diawe:article")
+            # return render(request, 'diawe/article.html')
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        log_form = LogForm()
+        # 赋值上下文
+        context = { 'log_form': log_form }
+        # 返回模板
+        return render(request, 'diawe/create.html', context)
